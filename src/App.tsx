0fa026f397ca90328bdc33d9c13c00ef6d522aff
Maryam <maryamjaved.xo@gmail.com>
2023-07-23 15:13:19 +0500

    alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import ListGroup from "./Components/ListGroup";
+// list group
+
+// import ListGroup from "./Components/ListGroup";
+
+// function App() {
+//     let items = [
+//         "New York",
+//         "sans francisco",
+//         "Tokyo",
+//         "London",
+//         "Paris"
+//     ];
+
+//     const handleSelectedItem = (item : string) => {
+//         console.log(item);
+//     }
+
+//     return (
+//         <div>
+//             <ListGroup items={items}
+//                 heading="Cities"
+//                 onSelectedItem={handleSelectedItem}/>
+//         </div>
+//     );
+// }
+
+import Alert from "./Components/alert";
 
 function App() {
-    let items = [
-        "New York",
-        "sans francisco",
-        "Tokyo",
-        "London",
-        "Paris"
-    ];
-
-    const handleSelectedItem = (item : string) => {
-        console.log(item);
-    }
-
     return (
         <div>
-            <ListGroup items={items}
-                heading="Cities"
-                onSelectedItem={handleSelectedItem}/>
+            <Alert/>
         </div>
     );
 }
-
 export default App;
